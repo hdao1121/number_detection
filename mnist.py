e6fd1aa0ee69de4b3c2df89e4d304c767c51fb29
hdao1121 <huy.dao13@gmail.com>
2018-07-06 13:55:19 -0400

    update

--- mnist.py
+++ mnist.py
@@ -5,7 +5,11 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
 from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from sklearn.ensemble import RandomForestClassifier
 import matplotlib
 import matplotlib.pyplot as plt
 import os.path
@@ -50,26 +54,51 @@ def show_random_digit(index):
     plt.show()
 
 def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+    print(precisions[:-1].shape, precisions.shape)
     plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
-    plt.plot(thresholds, recalls[:1], "g-", label="Recall")
+    plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
     plt.xlabel("Threshold")
     plt.legend(loc="upper left")
     plt.ylim([0, 1])
+    plt.show()
 
-def binary_classification(number):
+def plot_roc_curve(fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.axis([0, 1, 0, 1])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.show()
+
+def binary_classification(number, randomState):
     y_bin_train = (y_train == number)
     y_bin_test = (y_train != number)
 
-    sgd_clf = SGDClassifier(max_iter=5, tol=None)
-    sgd_clf.fit(X_train, y_bin_train)
+    # # SGDClassifier
+    # sgd_clf = SGDClassifier(max_iter=5, tol=None, random_state=randomState)
+    # sgd_clf.fit(X_train, y_bin_train)
+    # y_scores_sgd = cross_val_predict(sgd_clf, X_train, y_bin_train, cv=3, method="decision_function")
+    #
+    # # 90% precision, threshold is determined after looking through the plots
+    # precisions, recalls, thresholds = precision_recall_curve(y_bin_train, y_scores_sgd)
+    # # plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+    # y_bin_train_pred_90 = (y_scores_sgd > 70000)
+    # print('Precision score with 70,000 threshold:', precision_score(y_bin_train, y_bin_train_pred_90), "\n")
 
-    # print("confusion matrix: \n",confusion_matrix(y_bin_train, y_train_pred), "\n")
-    # print("f1 score: ", f1_score(y_bin_train, y_train_pred), "\n")
+    # RandomForestClassifer
+    forest_clf = RandomForestClassifier(random_state=randomState)
+    y_probas_forest = cross_val_predict(forest_clf, X_train, y_bin_train, cv=3, method="predict_proba")
+    y_scores_forest = y_probas_forest[:, 1]  # score = proba of positive class
+    fpr_forest, tpr_forest, thresholds_forest = roc_curve(y_bin_train, y_scores_forest)
 
-    y_scores = cross_val_predict(sgd_clf, X_train, y_bin_train, cv=3, method="decision_function")
-    precisions, recalls, thresholds = precision_recall_curve(y_bin_train, y_scores)
+    # ROC curve
+    fpr_sgd, tpr_sdg, thresholds_sgd = roc_curve(y_bin_train, y_scores_sgd)
+    # plot_roc_curve(fpr, tpr, thresholds)
+    print("ROC score for SDGClassifer:", roc_auc_score(y_bin_train, y_scores_sgd))
 
-binary_classification(5)
+
+
+binary_classification(5, 42)
 
 
 
