9b7056eae13b0489e255780cb20a66971f925d4b
hdao1121 <huy.dao13@gmail.com>
2018-07-14 17:02:01 -0400

    update

--- mnist.py
+++ mnist.py
@@ -110,5 +110,14 @@ def analyze():
     sgd_clf = SGDClassifier(max_iter=5, tol=None)
     y_train_predict = cross_val_predict(sgd_clf, X_train_scaled, y_train, cv=3)
     conf_mx = confusion_matrix(y_train, y_train_predict)
-    print(conf_mx)
+
+    # plot correct predictions. if gray swatches stay on diagonal, the numbers are classified correctly
+    plt.matshow(conf_mx, cmap= plt.cm.gray)
+
+    # plot errors
+    row_sums = conf_mx.sum(axis=1, keepdims=True)
+    norm_conf_matrix = conf_mx/row_sums
+    np.fill_diagonal(norm_conf_matrix, 0)
+    plt.matshow(norm_conf_matrix, cmap=plt.cm.gray)
+    plt.show()
 analyze()
